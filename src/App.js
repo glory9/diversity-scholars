4b8c84bfbd17e4f2d4bb3a8208a05eef259ce194
Glory Adedayo <gloryadedayo10@gmail.com>
2020-07-29 02:36:22 -0500

    add logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './new_logo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Mad oh!</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Welcome to HBCU Diversity Scholars</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
